5423c112ee7eef5f392cd91318215d188e71edab
Mohammed Awais Memon <awaismemon2323@gmail.com>
2022-03-17 12:28:37 +0530

    Initial Commit

---
+++ analyser/src/main/java/io/polyrepo/PolyRepoAnalyserApplication.java
@@ -0,0 +1,20 @@
+package io.polyrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableFeignClients
+public class PolyRepoAnalyserApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyRepoAnalyserApplication.class, args);
+	}
+
+}
---
+++ analyser/src/test/java/io/polyrepo/PolyRepoAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.polyrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyRepoAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
