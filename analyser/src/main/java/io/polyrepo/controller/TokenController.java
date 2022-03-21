8e1cd8e1111cc3f900e01484ec6aa15ed3ccdfe2
Mohammed Awais Memon <awaismemon2323@gmail.com>
2022-03-21 17:53:37 +0530

    feat: add controller for token

---
+++ analyser/src/main/java/io/polyrepo/controller/TokenController.java
@@ -0,0 +1,29 @@
+package io.polyrepo.controller;
+
+import io.polyrepo.constant.ApplicationConstants;
+import io.polyrepo.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/polyrepo/analyser")
+public class TokenController {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private ApplicationConstants constants;
+
+
+    @CrossOrigin
+    @GetMapping("/auth/{token}")
+    public String getToken(@PathVariable String token){
+
+        if(tokenService.validateToken(token)!=null){
+            constants.setToken(token);
+        }
+        return "Success";
+    }
+
+}
