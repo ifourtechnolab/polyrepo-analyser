06cc089e91e5c0e8da6bba6c4d8d5f198af39a48
Mohammed Awais Memon <awaismemon2323@gmail.com>
2022-03-22 16:06:01 +0530

    fix : remove unused imports

--- analyser/src/main/java/io/polyrepo/PolyRepoAnalyserApplication.java
+++ analyser/src/main/java/io/polyrepo/PolyRepoAnalyserApplication.java
@@ -3,8 +3,6 @@ package io.polyrepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
