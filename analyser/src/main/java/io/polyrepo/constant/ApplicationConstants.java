cbc45b76d699d2d9ad40d4d7187ca845f6ee32c0
Mohammed Awais Memon <awaismemon2323@gmail.com>
2022-03-22 11:50:18 +0530

    feat: make ApplicationConstants singleton

--- analyser/src/main/java/io/polyrepo/constant/ApplicationConstants.java
+++ analyser/src/main/java/io/polyrepo/constant/ApplicationConstants.java
@@ -5,7 +5,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApplicationConstants {
 
+    private static ApplicationConstants constants = null;
     private String token;
+
+
+    private ApplicationConstants() {}
+
+    public static ApplicationConstants getInstance(){
+        if(constants==null){
+            constants = new ApplicationConstants();
+        }
+        return constants;
+    }
+
+
 
     public String getToken() {
         return token;
