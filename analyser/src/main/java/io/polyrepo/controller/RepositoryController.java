477070da9f11c64cf05417ddcc7c1c920eef16c3
poojamodi18 <pooja18modi@gmail.com>
2022-03-24 17:24:08 +0530

    fix: repository endpoint return types

--- analyser/src/main/java/io/polyrepo/controller/RepositoryController.java
+++ analyser/src/main/java/io/polyrepo/controller/RepositoryController.java
@@ -3,6 +3,7 @@ package io.polyrepo.controller;
 import io.polyrepo.service.RepositoryService;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -15,21 +16,18 @@ public class RepositoryController {
     private RepositoryService repositoryService;
 
     @GetMapping("/org/{orgUserName}/repo")
-    public Map<String,Object> getRepositories(@PathVariable String orgUserName, @RequestHeader("Authorization") String token){
-        JSONObject result = new JSONObject(repositoryService.getRepositories(orgUserName,token));
-        return result.toMap();
+    public ResponseEntity<?> getRepositories(@PathVariable String orgUserName, @RequestHeader("Authorization") String token){
+        return repositoryService.getRepositories(orgUserName,token);
     }
 
     @GetMapping("/org/{orgUserName}/repo/more")
-    public Map<String,Object> getRepositories(@PathVariable String orgUserName, @RequestHeader("Authorization") String token,@RequestHeader("EndCursor") String endCursor){
-        JSONObject result = new JSONObject(repositoryService.getRepositoriesByCursor(orgUserName,token,endCursor));
-        return result.toMap();
+    public ResponseEntity<?> getRepositories(@PathVariable String orgUserName, @RequestHeader("Authorization") String token,@RequestHeader("EndCursor") String endCursor){
+        return repositoryService.getRepositoriesByCursor(orgUserName,token,endCursor);
     }
 
     @GetMapping("/org/{orgUserName}/repo/{repoName}")
-    public Map<String,Object> getRepositoriesByName(@PathVariable String orgUserName,@PathVariable String repoName ,@RequestHeader("Authorization") String token){
-        JSONObject result = new JSONObject(repositoryService.getRepositoriesByName(orgUserName,token,repoName));
-        return result.toMap();
+    public ResponseEntity<?> getRepositoriesByName(@PathVariable String orgUserName,@PathVariable String repoName ,@RequestHeader("Authorization") String token){
+        return repositoryService.getRepositoriesByName(orgUserName,token,repoName);
     }
 
 }
--- analyser/src/main/java/io/polyrepo/service/RepositoryService.java
+++ analyser/src/main/java/io/polyrepo/service/RepositoryService.java
@@ -1,9 +1,14 @@
 package io.polyrepo.service;
 
+import feign.FeignException;
 import io.polyrepo.client.GraphQLClient;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class RepositoryService {
@@ -12,35 +17,66 @@ public class RepositoryService {
     @Autowired
     private GraphQLClient client;
 
-    public String getRepositories(String orgUserName, String token) {
-        int count = getRepositoryCount(orgUserName,token);
-        if(count<=100) {
-            String query = "{\"query\":\"query { organization(login: \\\"" + orgUserName + "\\\") { repositories(first: " + count + ") { edges { repository:node { name } } } } }\"}";
-            JSONObject response = new JSONObject(client.getQuery("Bearer " + token, query));
-            return response.getJSONObject("data").getJSONObject("organization").toString();
-        }
-        else{
-            String query = "{\"query\":\"query { organization(login: \\\"" + orgUserName + "\\\") { repositories(first: 100) { edges { repository:node { name } }pageInfo{endCursor, hasNextPage} } } }\"}";
-            JSONObject response = new JSONObject(client.getQuery("Bearer " + token, query));
-            return response.getJSONObject("data").getJSONObject("organization").toString();
-        }
-    }
-
     private int getRepositoryCount(String orgUserName, String token) {
-        String query = "{\"query\":\"query { organization(login: \\\""+orgUserName+"\\\") { repositories{ totalCount } } }\"}";
-        JSONObject response = new JSONObject(client.getQuery("Bearer "+token, query));
-        return response.getJSONObject("data").getJSONObject("organization").getJSONObject("repositories").getInt("totalCount");
+        String query = "{\"query\":\"query { organization(login: \\\"" + orgUserName + "\\\") { repositories{ totalCount } } }\"}";
+        ResponseEntity<String> response = client.getQuery("Bearer " + token, query);
+        return new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("organization").getJSONObject("repositories").getInt("totalCount");
     }
 
-    public String getRepositoriesByCursor(String orgUserName, String token, String endCursor) {
-        String query = "{\"query\":\"query { organization(login: \\\"" + orgUserName + "\\\") { repositories(first: 100,after:\\\""+endCursor+"\\\") { edges { repository:node { name } }pageInfo{endCursor, hasNextPage} } } }\"}";
-        JSONObject response = new JSONObject(client.getQuery("Bearer " + token, query));
-        return response.getJSONObject("data").getJSONObject("organization").toString();
+    public ResponseEntity<?> getRepositories(String orgUserName, String token) {
+        ResponseEntity<String> response;
+        try {
+            int count = getRepositoryCount(orgUserName, token);
+            if (count <= 100) {
+                String query = "{\"query\":\"query { organization(login: \\\"" + orgUserName + "\\\") { repositories(first: " + count + ") { edges { repository:node { name } } } } }\"}";
+                response = client.getQuery("Bearer " + token, query);
+                JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("organization").getJSONObject("repositories");
+                return new ResponseEntity<>(result.toMap(), HttpStatus.OK);
+
+            } else {
+                String query = "{\"query\":\"query { organization(login: \\\"" + orgUserName + "\\\") { repositories(first: 100) { edges { repository:node { name } }pageInfo{endCursor, hasNextPage} } } }\"}";
+                response = client.getQuery("Bearer " + token, query);
+                JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("organization").getJSONObject("repositories");
+                return new ResponseEntity<>(result.toMap(), HttpStatus.OK);
+            }
+        } catch (FeignException.Unauthorized e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(Collections.singletonMap("edges", "Unauthorized"), HttpStatus.UNAUTHORIZED);
+        } catch (FeignException.BadRequest e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(Collections.singletonMap("edges", "Bad Request"), HttpStatus.BAD_REQUEST);
+        }
     }
 
-    public String getRepositoriesByName(String orgUserName, String token, String repoName) {
-        String query = "{\"query\":\"query {search(query: \\\"is:public org:"+orgUserName+" "+repoName+" in:name\\\", type: REPOSITORY, first: 30) {edges {node {... on Repository {name}}}}}\"}";
-        JSONObject response = new JSONObject(client.getQuery("Bearer " + token, query));
-        return response.getJSONObject("data").getJSONObject("search").toString();
+    public ResponseEntity<?> getRepositoriesByCursor(String orgUserName, String token, String endCursor) {
+        String query = "{\"query\":\"query { organization(login: \\\"" + orgUserName + "\\\") { repositories(first: 100,after:\\\"" + endCursor + "\\\") { edges { repository:node { name } }pageInfo{endCursor, hasNextPage} } } }\"}";
+        ResponseEntity<String> response;
+        try {
+            response = client.getQuery("Bearer " + token, query);
+            JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("organization").getJSONObject("repositories");
+            return new ResponseEntity<>(result.toMap(), HttpStatus.OK);
+        }catch (FeignException.Unauthorized e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(Collections.singletonMap("edges", "Unauthorized"), HttpStatus.UNAUTHORIZED);
+        } catch (FeignException.BadRequest e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(Collections.singletonMap("edges", "Bad Request"), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity<?> getRepositoriesByName(String orgUserName, String token, String repoName) {
+        String query = "{\"query\":\"query {search(query: \\\"is:public org:" + orgUserName + " " + repoName + " in:name\\\", type: REPOSITORY, first: 30) {edges {node {... on Repository {name}}}}}\"}";
+        ResponseEntity<String> response;
+        try {
+            response = client.getQuery("Bearer " + token, query);
+            JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("search");
+            return new ResponseEntity<>(result.toMap(), HttpStatus.OK);
+        }catch (FeignException.Unauthorized e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(Collections.singletonMap("edges", "Unauthorized"), HttpStatus.UNAUTHORIZED);
+        } catch (FeignException.BadRequest e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(Collections.singletonMap("edges", "Bad Request"), HttpStatus.BAD_REQUEST);
+        }
     }
 }
