daf1eb4d0b23e99820744480a50d6ad870f8f944
poojamodi18 <pooja18modi@gmail.com>
2022-03-29 12:05:33 +0530

    fix: removed unused imports

--- analyser/src/main/java/io/polyrepo/analyser/controller/TokenController.java
+++ analyser/src/main/java/io/polyrepo/analyser/controller/TokenController.java
@@ -2,18 +2,15 @@ package io.polyrepo.analyser.controller;
 
 import io.polyrepo.analyser.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 
 @RestController
 public class TokenController {
 
     @Autowired
     private TokenService tokenService;
-
 
     @CrossOrigin
     @GetMapping("/auth")
