eec98c471f8de0010bbc496e06f6250985b86845
Mohammed Awais Memon <awaismemon2323@gmail.com>
2022-03-31 15:15:34 +0530

    fix : add model in service class method

--- analyser/src/main/java/io/polyrepo/analyser/service/IssueService.java
+++ analyser/src/main/java/io/polyrepo/analyser/service/IssueService.java
@@ -2,6 +2,8 @@ package io.polyrepo.analyser.service;
 
 import feign.FeignException;
 import io.polyrepo.analyser.client.GraphQLClient;
+import io.polyrepo.analyser.model.RepoNamesList;
+import io.polyrepo.analyser.model.RepoName;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,14 +14,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 @Service
 public class IssueService {
 
     @Value("${getIssueWithPriority1Query}")
     private String getIssueWithPriority1Query;
+
+    @Value("${getClosedP1IssuesTimeQuery}")
+    private String getClosedP1IssuesTimeQuery;
+
+    @Value("${getClosedP2IssuesTimeQuery}")
+    private String getClosedP2IssuesTimeQuery;
 
     @Autowired
     private GraphQLClient client;
@@ -30,20 +36,20 @@ public class IssueService {
      * and the date they were created
      * @param orgUserName GitHub Organization login name
      * @param token       GitHub personal access token
-     * @param repoList    List of Repositories selected by user
+     * @param repoNamesList    List of Repositories selected by user
      * @return            List of priority-1 issues open since x date from selected repositories
      */
-    public ResponseEntity<?> getIssueWithPriority1(String orgUserName, String token, List<Map<String, String>> repoList) {
+    public ResponseEntity<?> getIssueWithPriority1(String orgUserName, String token, RepoNamesList repoNamesList) {
         StringBuilder repoNamesString = new StringBuilder();
-        for (Map<String,String> r:
-             repoList) {
-            repoNamesString.append("repo:").append(orgUserName).append("/").append(r.get("name")).append(" ");
+        for (RepoName r:
+             repoNamesList.getRepoNames()) {
+            repoNamesString.append("repo:").append(orgUserName).append("/").append(r.getName()).append(" ");
         }
-        if(repoList.size()==0){ repoNamesString.append("org:").append(orgUserName);}
+        if(repoNamesList.getRepoNames().isEmpty()){ repoNamesString.append("org:").append(orgUserName);}
 
         String query = String.format(getIssueWithPriority1Query,repoNamesString);
         LOG.info("Getting priority-1 issues from selected repositories of organization : "+orgUserName);
-        LOG.info("List of selected repositories : "+repoList);
+        LOG.info("List of selected repositories : "+repoNamesList);
 
         ResponseEntity<String> response;
         try {
