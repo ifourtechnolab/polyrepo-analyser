c617ed4c4d8316e7a21fb15101f0746bbc516d01
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-04-04 13:09:56 +0530

    fix: issue service class return value

--- analyser/src/main/java/io/polyrepo/analyser/controller/IssueController.java
+++ analyser/src/main/java/io/polyrepo/analyser/controller/IssueController.java
@@ -1,10 +1,17 @@
 package io.polyrepo.analyser.controller;
 
+import feign.FeignException;
 import io.polyrepo.analyser.model.RepoNamesList;
 import io.polyrepo.analyser.service.IssueService;
+import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
 
 
 @RestController
@@ -14,21 +21,47 @@ public class IssueController {
     @Autowired
     private IssueService issueService;
 
+    private final Logger LOG = LoggerFactory.getLogger(IssueController.class);
+
     @CrossOrigin
     @PostMapping("/{orgUserName}/repo/issuesWithPriority1")
     public ResponseEntity<?> getIssueWithPriority1(@PathVariable String orgUserName, @RequestHeader("Authorization") String token, @RequestBody RepoNamesList repoNamesList) {
-        return issueService.getIssueWithPriority1(orgUserName, token, repoNamesList);
+        try {
+            return new ResponseEntity<>(issueService.getIssueWithPriority1(orgUserName, token, repoNamesList), HttpStatus.OK);
+        } catch (FeignException.Unauthorized e) {
+            LOG.error(e.getMessage());
+            return new ResponseEntity<>(Collections.singletonMap("message", "Unauthorized"), HttpStatus.UNAUTHORIZED);
+        } catch (FeignException.BadRequest | JSONException e) {
+            LOG.error(e.getMessage());
+            return new ResponseEntity<>(Collections.singletonMap("message", "Bad Request"), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @CrossOrigin
     @GetMapping("/{orgUserName}/closedP1IssuesTime")
-    public ResponseEntity<?> getClosedP1IssuesTime(@PathVariable String orgUserName, @RequestHeader("Authorization") String token){
-        return issueService.getClosedP1IssuesTime(orgUserName,token);
+    public ResponseEntity<?> getClosedP1IssuesTime(@PathVariable String orgUserName, @RequestHeader("Authorization") String token) {
+        try {
+            return new ResponseEntity<>(issueService.getClosedP1IssuesTime(orgUserName, token), HttpStatus.OK);
+        } catch (FeignException.Unauthorized e) {
+            LOG.error(e.getMessage());
+            return new ResponseEntity<>(Collections.singletonMap("message", "Unauthorized"), HttpStatus.UNAUTHORIZED);
+        } catch (FeignException.BadRequest | JSONException e) {
+            LOG.error(e.getMessage());
+            return new ResponseEntity<>(Collections.singletonMap("message", "Bad Request"), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @CrossOrigin
     @GetMapping("/{orgUserName}/closedP2IssuesTime")
-    public ResponseEntity<?> getClosedP2IssuesTime(@PathVariable String orgUserName, @RequestHeader("Authorization") String token){
-        return issueService.getClosedP2IssuesTime(orgUserName,token);
+    public ResponseEntity<?> getClosedP2IssuesTime(@PathVariable String orgUserName, @RequestHeader("Authorization") String token) {
+        try {
+            return new ResponseEntity<>(issueService.getClosedP2IssuesTime(orgUserName, token), HttpStatus.OK);
+        } catch (FeignException.Unauthorized e) {
+            LOG.error(e.getMessage());
+            return new ResponseEntity<>(Collections.singletonMap("message", "Unauthorized"), HttpStatus.UNAUTHORIZED);
+        } catch (FeignException.BadRequest | JSONException e) {
+            LOG.error(e.getMessage());
+            return new ResponseEntity<>(Collections.singletonMap("message", "Bad Request"), HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- analyser/src/main/java/io/polyrepo/analyser/service/IssueService.java
+++ analyser/src/main/java/io/polyrepo/analyser/service/IssueService.java
@@ -4,16 +4,16 @@ import feign.FeignException;
 import io.polyrepo.analyser.client.GraphQLClient;
 import io.polyrepo.analyser.model.RepoNamesList;
 import io.polyrepo.analyser.model.RepoName;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.Map;
 
 @Service
 public class IssueService {
@@ -32,85 +32,74 @@ public class IssueService {
 
     private final Logger LOG = LoggerFactory.getLogger(IssueService.class);
 
-    /**This method returns the list of priority-1 issues of the selected repositories by user
+    /**
+     * This method returns the list of priority-1 issues of the selected repositories by user
      * and the date they were created
-     * @param orgUserName GitHub Organization login name
-     * @param token       GitHub personal access token
-     * @param repoNamesList    List of Repositories selected by user
-     * @return            List of priority-1 issues open since x date from selected repositories
+     *
+     * @param orgUserName   GitHub Organization login name
+     * @param token         GitHub personal access token
+     * @param repoNamesList List of Repositories selected by user
+     * @return List of priority-1 issues open since x date from selected repositories
      */
-    public ResponseEntity<?> getIssueWithPriority1(String orgUserName, String token, RepoNamesList repoNamesList) {
+    public Map<String, Object> getIssueWithPriority1(String orgUserName, String token, RepoNamesList repoNamesList) throws FeignException, JSONException {
         StringBuilder repoNamesString = new StringBuilder();
-        for (RepoName r:
-             repoNamesList.getRepoNames()) {
+        for (RepoName r :
+                repoNamesList.getRepoNames()) {
             repoNamesString.append("repo:").append(orgUserName).append("/").append(r.getName()).append(" ");
         }
-        if(repoNamesList.getRepoNames().isEmpty()){ repoNamesString.append("org:").append(orgUserName);}
+        if (repoNamesList.getRepoNames().isEmpty()) {
+            repoNamesString.append("org:").append(orgUserName);
+        }
 
-        String query = String.format(getIssueWithPriority1Query,repoNamesString);
-        LOG.info("Getting priority-1 issues from selected repositories of organization : "+orgUserName);
-        LOG.info("List of selected repositories : "+repoNamesList);
+        String query = String.format(getIssueWithPriority1Query, repoNamesString);
+        LOG.info("Getting priority-1 issues from selected repositories of organization : " + orgUserName);
+        LOG.info("List of selected repositories : " + repoNamesList);
 
         ResponseEntity<String> response;
-        try {
-            response = client.getQuery("Bearer " + token, query);
-            JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("search");
-            return new ResponseEntity<>(result.toMap(), HttpStatus.OK);
-        }catch (FeignException.Unauthorized e){
-            LOG.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap("message","Unauthorized"),HttpStatus.UNAUTHORIZED);
-        }catch (FeignException.BadRequest e){
-            LOG.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap("message","Bad Request"),HttpStatus.BAD_REQUEST);
-        }
+
+        response = client.getQuery("Bearer " + token, query);
+        JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("search");
+        return result.toMap();
     }
 
-    /**This method returns the list of priority-1 issue creation and closing time through
+    /**
+     * This method returns the list of priority-1 issue creation and closing time through
      * which average time taken to resolve priority-1 issues can be obtained
+     *
      * @param orgUserName GitHub Organization login name
      * @param token       GitHub personal access token
-     * @return            List of priority-1 issues' creation and closing time
+     * @return List of priority-1 issues' creation and closing time
      */
-    public ResponseEntity<?> getClosedP1IssuesTime(String orgUserName, String token) {
-        String query = String.format(getClosedP1IssuesTimeQuery,orgUserName);
-        LOG.info("Getting creation and closing time of priority-1 issues of organization : "+orgUserName);
+    public Map<String, Object> getClosedP1IssuesTime(String orgUserName, String token) throws FeignException, JSONException {
+        String query = String.format(getClosedP1IssuesTimeQuery, orgUserName);
+        LOG.info("Getting creation and closing time of priority-1 issues of organization : " + orgUserName);
 
         ResponseEntity<String> response;
-        try {
-            response = client.getQuery("Bearer " + token, query);
-            JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("search");
-            return new ResponseEntity<>(result.toMap(), HttpStatus.OK);
-        }catch (FeignException.Unauthorized e){
-            LOG.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap("message","Unauthorized"),HttpStatus.UNAUTHORIZED);
-        }catch (FeignException.BadRequest e){
-            LOG.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap("message","Bad Request"),HttpStatus.BAD_REQUEST);
-        }
+
+        response = client.getQuery("Bearer " + token, query);
+        JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("search");
+        return result.toMap();
+
     }
 
-    /**This method returns the list of priority-2 issue creation and closing time through
+    /**
+     * This method returns the list of priority-2 issue creation and closing time through
      * which average time taken to resolve priority-2 issues can be obtained
+     *
      * @param orgUserName GitHub Organization login name
      * @param token       GitHub personal access token
-     * @return            List of priority-2 issues' creation and closing time
+     * @return List of priority-2 issues' creation and closing time
      */
-    public ResponseEntity<?> getClosedP2IssuesTime(String orgUserName, String token) {
-        String query = String.format(getClosedP2IssuesTimeQuery,orgUserName);
-        LOG.info("Getting creation and closing time of priority-2 issues of organization : "+orgUserName);
+    public Map<String, Object> getClosedP2IssuesTime(String orgUserName, String token) throws FeignException, JSONException {
+        String query = String.format(getClosedP2IssuesTimeQuery, orgUserName);
+        LOG.info("Getting creation and closing time of priority-2 issues of organization : " + orgUserName);
 
         ResponseEntity<String> response;
-        try {
-            response = client.getQuery("Bearer " + token, query);
-            JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("search");
-            return new ResponseEntity<>(result.toMap(), HttpStatus.OK);
-        }catch (FeignException.Unauthorized e){
-            LOG.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap("message","Unauthorized"),HttpStatus.UNAUTHORIZED);
-        }catch (FeignException.BadRequest e){
-            LOG.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap("message","Bad Request"),HttpStatus.BAD_REQUEST);
-        }
+
+        response = client.getQuery("Bearer " + token, query);
+        JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("search");
+        return result.toMap();
+
     }
 }
 
