978e14f3c170a229766768457a21b4dc3d025217
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-04-04 12:59:24 +0530

    fix: repository service class return value

--- analyser/src/main/java/io/polyrepo/analyser/service/RepositoryService.java
+++ analyser/src/main/java/io/polyrepo/analyser/service/RepositoryService.java
@@ -8,11 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.Map;
 
 @Service
 public class RepositoryService {
@@ -40,98 +39,80 @@ public class RepositoryService {
 
     /**
      * Returns the total number of repositories the organization has
+     *
      * @param orgUserName GitHub Organization login name
      * @param token       GitHub personal access token
-     * @return            Total count of repositories of specified organization
+     * @return Total count of repositories of specified organization
      */
     private int getRepositoryCount(String orgUserName, String token) {
-        String query = String.format(getRepositoryCountQuery,orgUserName);
+        String query = String.format(getRepositoryCountQuery, orgUserName);
         ResponseEntity<String> response = client.getQuery("Bearer " + token, query);
         return new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("organization").getJSONObject("repositories").getInt("totalCount");
     }
 
-    /** This method returns the repository list according to the number of repositories of the organization
-     *  if the total number of repositories is greater than a hundred then
-     *  the query with pagination will get executed and return the list of repositories with pagination
+    /**
+     * This method returns the repository list according to the number of repositories of the organization
+     * if the total number of repositories is greater than a hundred then
+     * the query with pagination will get executed and return the list of repositories with pagination
+     *
      * @param orgUserName GitHub Organization login name
      * @param token       GitHub personal access token
-     * @return            List of repositories of specified organization
+     * @return List of repositories of specified organization
      */
-    public ResponseEntity<?> getRepositories(String orgUserName, String token) {
+    public Map<String, Object> getRepositories(String orgUserName, String token) throws FeignException, JSONException {
         ResponseEntity<String> response;
-        try {
-            int count = getRepositoryCount(orgUserName, token);
-            LOG.info("Total count of repositories: "+count);
-            if (count <= 100) {
-                LOG.info("Getting all the repositories");
-                String query = String.format(getRepositoriesQueryUnder100,orgUserName,count);
-                response = client.getQuery("Bearer " + token, query);
-                JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("organization").getJSONObject("repositories");
-                return new ResponseEntity<>(result.toMap(), HttpStatus.OK);
 
-            } else {
-                LOG.info("Getting first 100 repositories");
-                String query = String.format(getRepositoriesQueryOver100,orgUserName);
-                response = client.getQuery("Bearer " + token, query);
-                JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("organization").getJSONObject("repositories");
-                return new ResponseEntity<>(result.toMap(), HttpStatus.OK);
-            }
-        } catch (FeignException.Unauthorized e) {
-            LOG.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap("edges", "Unauthorized"), HttpStatus.UNAUTHORIZED);
-        } catch (FeignException.BadRequest | JSONException e) {
-            LOG.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap("edges", "Bad Request"), HttpStatus.BAD_REQUEST);
+        int count = getRepositoryCount(orgUserName, token);
+        LOG.info("Total count of repositories: " + count);
+        if (count <= 100) {
+            LOG.info("Getting all the repositories");
+            String query = String.format(getRepositoriesQueryUnder100, orgUserName, count);
+            response = client.getQuery("Bearer " + token, query);
+            JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("organization").getJSONObject("repositories");
+            return result.toMap();
+        } else {
+            LOG.info("Getting first 100 repositories");
+            String query = String.format(getRepositoriesQueryOver100, orgUserName);
+            response = client.getQuery("Bearer " + token, query);
+            JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("organization").getJSONObject("repositories");
+            return result.toMap();
         }
     }
 
     /**
      * This method fetches the continuing list of repositories of the specified organization using the end cursor of the
      * previous list of repositories and returns the fetched list with pagination
+     *
      * @param orgUserName GitHub Organization login name
      * @param token       GitHub personal access token
      * @param endCursor   End cursor of repository list json
-     * @return            List of Repository of specified organization with pagination
+     * @return List of Repository of specified organization with pagination
      */
-    public ResponseEntity<?> getRepositoriesByCursor(String orgUserName, String token, String endCursor) {
-        String query = String.format(getRepositoriesByCursorQuery,orgUserName,endCursor);
+    public Map<String, Object> getRepositoriesByCursor(String orgUserName, String token, String endCursor) throws FeignException, JSONException {
+        String query = String.format(getRepositoriesByCursorQuery, orgUserName, endCursor);
         ResponseEntity<String> response;
-        try {
-            LOG.info("Getting 100 repositories after the cursor \""+endCursor+"\"");
-            response = client.getQuery("Bearer " + token, query);
-            JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("organization").getJSONObject("repositories");
-            return new ResponseEntity<>(result.toMap(), HttpStatus.OK);
-        }catch (FeignException.Unauthorized e) {
-            LOG.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap("edges", "Unauthorized"), HttpStatus.UNAUTHORIZED);
-        } catch (FeignException.BadRequest | JSONException e) {
-            LOG.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap("edges", "Bad Request"), HttpStatus.BAD_REQUEST);
-        }
+
+        LOG.info("Getting 100 repositories after the cursor \"" + endCursor + "\"");
+        response = client.getQuery("Bearer " + token, query);
+        JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("organization").getJSONObject("repositories");
+        return result.toMap();
     }
 
     /**
      * This method fetches the list of repositories which are having the same name as the mentioned name
+     *
      * @param orgUserName GitHub Organization login name
      * @param token       GitHub personal access token
      * @param repoName    Repository name
-     * @return            List of repository of specified organization having the same name as specified name
+     * @return List of repository of specified organization having the same name as specified name
      */
-    public ResponseEntity<?> getRepositoriesByName(String orgUserName, String token, String repoName) {
-        String query = String.format(getRepositoriesByNameQuery,orgUserName,repoName);
+    public Map<String, Object> getRepositoriesByName(String orgUserName, String token, String repoName) throws FeignException, JSONException {
+        String query = String.format(getRepositoriesByNameQuery, orgUserName, repoName);
         ResponseEntity<String> response;
-        try {
-            LOG.info("Getting repositories by name : "+repoName);
-            response = client.getQuery("Bearer " + token, query);
-            JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("search");
-            return new ResponseEntity<>(result.toMap(), HttpStatus.OK);
-        }catch (FeignException.Unauthorized e) {
-            LOG.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap("edges", "Unauthorized"), HttpStatus.UNAUTHORIZED);
-        } catch (FeignException.BadRequest | JSONException e) {
-            LOG.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap("edges", "Bad Request"), HttpStatus.BAD_REQUEST);
-        }
+        LOG.info("Getting repositories by name : " + repoName);
+        response = client.getQuery("Bearer " + token, query);
+        JSONObject result = new JSONObject(response.getBody()).getJSONObject("data").getJSONObject("search");
+        return result.toMap();
     }
 
 }
