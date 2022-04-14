981d96d9d5467404e7dfdbbb6a356dbc0495c441
Mohammed Awais Memon <awaismemon2323@gmail.com>
2022-04-14 12:33:36 +0530

    fix : change in response data of P1 & P2 time
    description : instead of returning list of creation and closing time of issues the endpoints will return the calculated average.

--- src/main/java/io/polyrepo/analyser/service/IssueService.java
+++ src/main/java/io/polyrepo/analyser/service/IssueService.java
@@ -6,6 +6,7 @@ import io.polyrepo.analyser.constant.StringConstants;
 import io.polyrepo.analyser.model.RepoNamesList;
 import io.polyrepo.analyser.util.DateUtil;
 import io.polyrepo.analyser.util.QueryUtil;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -15,8 +16,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @Service
 public class IssueService {
@@ -37,7 +37,7 @@ public class IssueService {
 
     /**
      * This method returns the list of priority-1 issues of the selected repositories by user
-     * and the date they were created
+     * and the date they were created with the name of repository they belong to
      *
      * @param orgUserName   GitHub Organization login name
      * @param token         GitHub personal access token
@@ -63,47 +63,71 @@ public class IssueService {
     }
 
     /**
-     * This method returns the list of priority-1 issue creation and closing time through
-     * which average time taken to resolve priority-1 issues can be obtained
+     * This method returns the average time taken to resolve priority-1 issues
      *
      * @param orgUserName GitHub Organization login name
      * @param token       GitHub personal access token
-     * @return List of priority-1 issues' creation and closing time
+     * @return Average time taken to resolve priority-1 issues
      * @throws FeignException FeignException.Unauthorized if token is invalid, FeignException.BadRequest if FeignClient returns 400 Bad Request
      * @throws JSONException if JSON parsing is invalid
      */
-    public Map<String, Object> getClosedP1IssuesTime(String orgUserName, String token) throws FeignException, JSONException {
+    public Map<String, String> getAverageResolvingTimeOfP1Issues(String orgUserName, String token) throws FeignException, JSONException {
         String query = String.format(getClosedP1IssuesTimeQuery, orgUserName);
         logger.info("Getting creation and closing time of priority-1 issues of organization : {}", orgUserName);
 
         ResponseEntity<String> response;
 
         response = client.getQuery(StringConstants.AUTH_HEADER_PREFIX + token, query);
-        JSONObject result = new JSONObject(Objects.requireNonNull(response.getBody())).getJSONObject(StringConstants.JSON_DATA_KEY).getJSONObject(StringConstants.JSON_SEARCH_KEY);
-        return result.toMap();
+        JSONArray edges = new JSONObject(Objects.requireNonNull(response.getBody())).getJSONObject(StringConstants.JSON_DATA_KEY).getJSONObject(StringConstants.JSON_SEARCH_KEY).getJSONArray("edges");
 
+        List<Long> timeDiffList = new ArrayList<>();
+        for(int i = 0; i < edges.length(); i++){
+            String createdAt = edges.getJSONObject(i).getJSONObject("node").getString("createdAt");
+            String closedAt = edges.getJSONObject(i).getJSONObject("node").getString("closedAt");
+            timeDiffList.add(DateUtil.calculateDiffBetweenDates(createdAt,closedAt));
+        }
+
+        logger.info("List of time difference : {}",timeDiffList);
+        if(timeDiffList.isEmpty()){
+            return Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"No average because there are no issues");
+        }
+        else {
+            return Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,DateUtil.calculateAverage(timeDiffList));
+        }
     }
 
     /**
-     * This method returns the list of priority-2 issue creation and closing time through
-     * which average time taken to resolve priority-2 issues can be obtained
+     * This method returns the average time taken to resolve priority-2 issues
      *
      * @param orgUserName GitHub Organization login name
      * @param token       GitHub personal access token
-     * @return List of priority-2 issues' creation and closing time
+     * @return Average time taken to resolve priority-2 issues
      * @throws FeignException FeignException.Unauthorized if token is invalid, FeignException.BadRequest if FeignClient returns 400 Bad Request
      * @throws JSONException if JSON parsing is invalid
      */
-    public Map<String, Object> getClosedP2IssuesTime(String orgUserName, String token) throws FeignException, JSONException {
+    public Map<String, String> getAverageResolvingTimeOfP2Issues(String orgUserName, String token) throws FeignException, JSONException {
         String query = String.format(getClosedP2IssuesTimeQuery, orgUserName);
         logger.info("Getting creation and closing time of priority-2 issues of organization : {}", orgUserName);
 
         ResponseEntity<String> response;
 
         response = client.getQuery(StringConstants.AUTH_HEADER_PREFIX + token, query);
-        JSONObject result = new JSONObject(Objects.requireNonNull(response.getBody())).getJSONObject(StringConstants.JSON_DATA_KEY).getJSONObject(StringConstants.JSON_SEARCH_KEY);
-        return result.toMap();
+        JSONArray edges = new JSONObject(Objects.requireNonNull(response.getBody())).getJSONObject(StringConstants.JSON_DATA_KEY).getJSONObject(StringConstants.JSON_SEARCH_KEY).getJSONArray("edges");
 
+        List<Long> timeDiffList = new ArrayList<>();
+        for(int i = 0; i < edges.length(); i++){
+            String createdAt = edges.getJSONObject(i).getJSONObject("node").getString("createdAt");
+            String closedAt = edges.getJSONObject(i).getJSONObject("node").getString("closedAt");
+            timeDiffList.add(DateUtil.calculateDiffBetweenDates(createdAt,closedAt));
+        }
+
+        logger.info("List of time difference : {}",timeDiffList);
+        if(timeDiffList.isEmpty()){
+            return Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"No average because there are no issues");
+        }
+        else {
+            return Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,DateUtil.calculateAverage(timeDiffList));
+        }
     }
 }
 
