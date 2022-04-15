d3479fed20dc59a568d16a70ed8cc2cdc7e95455
Mohammed Awais Memon <awaismemon2323@gmail.com>
2022-04-15 12:00:58 +0530

    Sonar fix : reduce code duplication

--- src/main/java/io/polyrepo/analyser/service/IssueService.java
+++ src/main/java/io/polyrepo/analyser/service/IssueService.java
@@ -4,9 +4,9 @@ import feign.FeignException;
 import io.polyrepo.analyser.client.GraphQLClient;
 import io.polyrepo.analyser.constant.StringConstants;
 import io.polyrepo.analyser.model.RepoNamesList;
+import io.polyrepo.analyser.util.AverageUtil;
 import io.polyrepo.analyser.util.DateUtil;
 import io.polyrepo.analyser.util.QueryUtil;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -75,25 +75,8 @@ public class IssueService {
         String query = String.format(getClosedP1IssuesTimeQuery, orgUserName);
         logger.info("Getting creation and closing time of priority-1 issues of organization : {}", orgUserName);
 
-        ResponseEntity<String> response;
-
-        response = client.getQuery(StringConstants.AUTH_HEADER_PREFIX + token, query);
-        JSONArray edges = new JSONObject(Objects.requireNonNull(response.getBody())).getJSONObject(StringConstants.JSON_DATA_KEY).getJSONObject(StringConstants.JSON_SEARCH_KEY).getJSONArray("edges");
-
-        List<Long> timeDiffList = new ArrayList<>();
-        for(int i = 0; i < edges.length(); i++){
-            String createdAt = edges.getJSONObject(i).getJSONObject("node").getString("createdAt");
-            String closedAt = edges.getJSONObject(i).getJSONObject("node").getString("closedAt");
-            timeDiffList.add(DateUtil.calculateDiffBetweenDates(createdAt,closedAt));
-        }
-
-        logger.info("List of time difference : {}",timeDiffList);
-        if(timeDiffList.isEmpty()){
-            return Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"No average because there are no issues");
-        }
-        else {
-            return Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,DateUtil.calculateAverage(timeDiffList));
-        }
+        ResponseEntity<String> response = client.getQuery(StringConstants.AUTH_HEADER_PREFIX + token, query);
+        return AverageUtil.getAverageTime(response);
     }
 
     /**
@@ -109,25 +92,8 @@ public class IssueService {
         String query = String.format(getClosedP2IssuesTimeQuery, orgUserName);
         logger.info("Getting creation and closing time of priority-2 issues of organization : {}", orgUserName);
 
-        ResponseEntity<String> response;
-
-        response = client.getQuery(StringConstants.AUTH_HEADER_PREFIX + token, query);
-        JSONArray edges = new JSONObject(Objects.requireNonNull(response.getBody())).getJSONObject(StringConstants.JSON_DATA_KEY).getJSONObject(StringConstants.JSON_SEARCH_KEY).getJSONArray("edges");
-
-        List<Long> timeDiffList = new ArrayList<>();
-        for(int i = 0; i < edges.length(); i++){
-            String createdAt = edges.getJSONObject(i).getJSONObject("node").getString("createdAt");
-            String closedAt = edges.getJSONObject(i).getJSONObject("node").getString("closedAt");
-            timeDiffList.add(DateUtil.calculateDiffBetweenDates(createdAt,closedAt));
-        }
-
-        logger.info("List of time difference : {}",timeDiffList);
-        if(timeDiffList.isEmpty()){
-            return Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"No average because there are no issues");
-        }
-        else {
-            return Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,DateUtil.calculateAverage(timeDiffList));
-        }
+        ResponseEntity<String> response = client.getQuery(StringConstants.AUTH_HEADER_PREFIX + token, query);
+        return AverageUtil.getAverageTime(response);
     }
 }
 
