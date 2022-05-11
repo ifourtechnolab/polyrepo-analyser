e4c826180aba3f0a996d14790822530e718430d7
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-11 12:15:27 +0530

    feat: token validation and update

--- src/main/java/io/polyrepo/analyser/service/UserService.java
+++ src/main/java/io/polyrepo/analyser/service/UserService.java
@@ -1,14 +1,18 @@
 package io.polyrepo.analyser.service;
 
+import feign.FeignException;
+import io.polyrepo.analyser.constant.StringConstants;
 import io.polyrepo.analyser.model.User;
 import io.polyrepo.analyser.repository.UserRepository;
+import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -18,15 +22,66 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    private TokenService tokenService;
+
     private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
-    public void save(User user) throws DuplicateKeyException {
+    public void save(User user) throws DuplicateKeyException, SQLException {
         userRepository.save(user);
     }
 
-    public Map<String, Object> login(String email, String password)  throws IndexOutOfBoundsException{
-        List<Map<String, Object>> userDetails = userRepository.findByEmailAndPassword(email, password);
-        logger.info("User Info: {}",userDetails);
-        return userDetails.get(0);
+    public Map<String, Object> updateToken(User user) {
+        Map<String, Object> response = new HashMap<>();
+        String responseValue;
+        try{
+            responseValue = tokenService.validateToken(user.getBearerToken());
+            if(responseValue.equals("Valid Token")) {
+                int returnValue = userRepository.updateToken(user.getId(), user.getBearerToken());
+                if (returnValue > 0) {
+                    response.put(StringConstants.JSON_MESSAGE_KEY_STRING, "Token updated");
+                } else {
+                    response.put(StringConstants.JSON_MESSAGE_KEY_STRING, "Token update process failed");
+                }
+            }
+            return response;
+        }catch (FeignException.Unauthorized e){
+            response.put(StringConstants.JSON_MESSAGE_KEY_STRING,"Invalid Token");
+            logger.error(e.getMessage());
+            return response;
+        }catch (FeignException.BadRequest | JSONException e){
+            response.put(StringConstants.JSON_MESSAGE_KEY_STRING,"Bad Request");
+            logger.error(e.getMessage());
+            return response;
+        }catch (SQLException e){
+            response.put(StringConstants.JSON_MESSAGE_KEY_STRING,e.getMessage());
+            logger.error(e.getMessage());
+            return response;
+        }
+    }
+
+    public Map<String, Object> login(String email, String password) throws IndexOutOfBoundsException {
+        User user = userRepository.findByEmailAndPassword(email, password);
+        Map<String, Object> userDetail = new HashMap<>();
+        if (user.getBearerToken() != null) {
+            userDetail.put("id", user.getId());
+            userDetail.put("bearer_token", user.getBearerToken());
+            String responseValue;
+            try{
+                responseValue = tokenService.validateToken(user.getBearerToken());
+            }catch (FeignException.Unauthorized e){
+                responseValue="Invalid Token";
+                logger.error(e.getMessage());
+            }catch (FeignException.BadRequest | JSONException e){
+                responseValue="Bad Request";
+                logger.error(e.getMessage());
+            }
+            userDetail.put("token_validation",responseValue);
+            userDetail.put(StringConstants.JSON_MESSAGE_KEY_STRING, "User Found");
+            logger.info("User Info: {} , {}", user.getId(),user.getBearerToken());
+        } else {
+            userDetail.put(StringConstants.JSON_MESSAGE_KEY_STRING, "User Not Found");
+        }
+        return userDetail;
     }
 }
