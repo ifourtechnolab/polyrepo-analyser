6a860d9e4f8bf29f12dbb576994f0ffa92eed676
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-11 12:13:32 +0530

    feat: update token endpoint

--- src/main/java/io/polyrepo/analyser/controller/UserController.java
+++ src/main/java/io/polyrepo/analyser/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Collections;
 import java.util.Map;
 
@@ -27,30 +28,31 @@ public class UserController {
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @PostMapping("/register")
-    public ResponseEntity<Map<String,String>> createUser(@RequestBody User user) {
+    public ResponseEntity<Map<String, String>> createUser(@RequestBody User user) {
         try {
             userService.save(new User(user.getUserName(), user.getEmail(), user.getBearerToken(), user.getPassword()));
-            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"User was created successfully."), HttpStatus.OK);
-        } catch (DuplicateKeyException e) {
+            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING, "User was created successfully."), HttpStatus.OK);
+        } catch (DuplicateKeyException | SQLIntegrityConstraintViolationException e) {
             logger.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"Email Already Used"), HttpStatus.OK);
-        }catch (Exception e) {
+            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING, "Email Already Used"), HttpStatus.OK);
+        } catch (Exception e) {
             logger.error(e.getMessage());
-            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,e.getMessage()), HttpStatus.OK);
+            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING, e.getMessage()), HttpStatus.OK);
         }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String,Object>> login(@RequestBody User user){
-        try{
-            Map<String, Object> response = userService.login(user.getEmail(), user.getPassword());
-            response.put(StringConstants.JSON_MESSAGE_KEY_STRING, "User Found");
-            return new ResponseEntity<>(response, HttpStatus.OK);
-
-        }catch (IndexOutOfBoundsException e){
+    public ResponseEntity<Map<String, Object>> login(@RequestBody User user) {
+        try {
+            return new ResponseEntity<>(userService.login(user.getEmail(), user.getPassword()), HttpStatus.OK);
+        } catch (IndexOutOfBoundsException e) {
             logger.error(e.getMessage());
             return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING, "User Not Found"), HttpStatus.OK);
         }
     }
 
+    @PostMapping("/updateToken")
+    public ResponseEntity<Map<String, Object>> updateToken(@RequestBody User user) {
+        return new ResponseEntity<>(userService.updateToken(user), HttpStatus.OK);
+    }
 }
