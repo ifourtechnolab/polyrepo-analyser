75721b01445a8b316f3a46f2a11f65619a21395e
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-11 18:07:34 +0530

    fix: lambok annotation

--- src/main/java/io/polyrepo/analyser/model/User.java
+++ src/main/java/io/polyrepo/analyser/model/User.java
@@ -1,14 +1,10 @@
 package io.polyrepo.analyser.model;
 
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
-@ToString
 public class User {
 
     private int id;
