df66cc9e5d720e35166054707329386c4ac0f25a
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-16 09:48:16 +0530

    fix: SQL exceptions

--- src/main/java/io/polyrepo/analyser/controller/UserController.java
+++ src/main/java/io/polyrepo/analyser/controller/UserController.java
@@ -1,8 +1,10 @@
 package io.polyrepo.analyser.controller;
 
+import feign.FeignException;
 import io.polyrepo.analyser.constant.StringConstants;
 import io.polyrepo.analyser.model.User;
 import io.polyrepo.analyser.service.UserService;
+import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Collections;
 import java.util.Map;
@@ -28,14 +31,19 @@ public class UserController {
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @PostMapping("/register")
-    public ResponseEntity<Map<String, String>> createUser(@RequestBody User user) {
+    public ResponseEntity<Map<String, Object>> createUser(@RequestBody User user) {
         try {
-            userService.save(new User(user.getUserName(), user.getEmail(), user.getBearerToken(), user.getPassword()));
-            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING, "User was created successfully."), HttpStatus.OK);
+            return new ResponseEntity<>(userService.save(new User(user.getUserName(), user.getEmail(), user.getBearerToken(), user.getPassword())), HttpStatus.OK);
         } catch (DuplicateKeyException | SQLIntegrityConstraintViolationException e) {
             logger.error(e.getMessage());
             return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING, "Email Already Used"), HttpStatus.OK);
-        } catch (Exception e) {
+        } catch (FeignException.Unauthorized e){
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"Invalid Token"),HttpStatus.OK);
+        }catch (FeignException.BadRequest | JSONException e){
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"Bad Request"),HttpStatus.OK);
+        }catch (Exception e) {
             logger.error(e.getMessage());
             return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING, e.getMessage()), HttpStatus.OK);
         }
@@ -53,6 +61,18 @@ public class UserController {
 
     @PostMapping("/updateToken")
     public ResponseEntity<Map<String, Object>> updateToken(@RequestBody User user) {
+        try{
         return new ResponseEntity<>(userService.updateToken(user), HttpStatus.OK);
+        }catch (FeignException.Unauthorized e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"Invalid Token"),HttpStatus.OK);
+        } catch (FeignException.BadRequest | JSONException e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"Bad Request"),HttpStatus.OK);
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING, e.getMessage()), HttpStatus.OK);
+        }
+
     }
 }
