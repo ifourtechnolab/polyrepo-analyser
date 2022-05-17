899d45acfca42e6e051fe8882b2dc4f57dec4f66
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-17 10:47:09 +0530

    feat: save query in database

--- src/main/java/io/polyrepo/analyser/repository/QueryRepository.java
+++ src/main/java/io/polyrepo/analyser/repository/QueryRepository.java
@@ -1,7 +1,10 @@
 package io.polyrepo.analyser.repository;
 
+import io.polyrepo.analyser.model.StoredQueries;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Repository;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -10,4 +13,6 @@ import java.util.Map;
 public interface QueryRepository {
 
     List<Map<String, Object>> getStoredQueries(int userId) throws IndexOutOfBoundsException;
+
+    int saveStoredQuery(StoredQueries storedQueries)throws DuplicateKeyException, SQLException;
 }
--- src/main/java/io/polyrepo/analyser/repository/QueryRepositoryImpl.java
+++ src/main/java/io/polyrepo/analyser/repository/QueryRepositoryImpl.java
@@ -1,9 +1,17 @@
 package io.polyrepo.analyser.repository;
 
+import io.polyrepo.analyser.model.StoredQueries;
+import io.polyrepo.analyser.util.ConnectionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -13,11 +21,41 @@ public class QueryRepositoryImpl implements QueryRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Value("${saveStoredQuery}")
+    private String saveStoredQuery;
+
+
     @Override
     public List<Map<String, Object>> getStoredQueries(int userId) throws IndexOutOfBoundsException {
         String sql = String.format("SELECT s.q_id, title, query, GROUP_CONCAT(name) as RepoNames, param1,param2\n" +
                 "FROM stored_queries as s, stored_repository_names\n" +
                 "WHERE s.q_id = '%1$s'",userId);
         return  jdbcTemplate.queryForList(sql);
+    }
+
+    /**
+     * This method will save stored query details in database
+     * @param storedQueries stored query details
+     * @return the primary key of stored query
+     * @throws DuplicateKeyException if data with same primary key exists in database
+     * @throws SQLException if error occurs in database operation
+     */
+    @Override
+    public int saveStoredQuery(StoredQueries storedQueries) throws DuplicateKeyException, SQLException {
+        try(Connection connection= ConnectionUtil.getConnection()){
+            try(PreparedStatement preparedStatement = connection.prepareStatement(saveStoredQuery,java.sql.Statement.RETURN_GENERATED_KEYS)){
+                preparedStatement.setString(1,storedQueries.getTitle());
+                preparedStatement.setString(2,storedQueries.getQueryKey());
+                preparedStatement.setInt(3,storedQueries.getUserId());
+                int returnVal= preparedStatement.executeUpdate();
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+                if(resultSet.next()){
+                    return resultSet.getInt(1);
+                }
+                else{
+                    return returnVal;
+                }
+            }
+        }
     }
 }
