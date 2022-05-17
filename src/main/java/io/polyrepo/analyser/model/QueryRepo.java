dd48c477e023b65c9295f26208db0ada49f1e693
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-17 14:37:41 +0530

    fix: model name

--- src/main/java/io/polyrepo/analyser/model/QueryRepo.java
+++ src/main/java/io/polyrepo/analyser/model/QueryRepo.java
@@ -5,12 +5,12 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class StoredRepository {
+public class QueryRepo {
     private int id;
     private String repoName;
     private int queryId;
 
-    public StoredRepository(String repoName, int queryId) {
+    public QueryRepo(String repoName, int queryId) {
         this.repoName = repoName;
         this.queryId = queryId;
     }
