5a821402bd330ad7cecfcaae7de97fe830b22bfc
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-17 10:54:10 +0530

    feat: query parameter model

---
+++ src/main/java/io/polyrepo/analyser/model/QueryParameter.java
@@ -0,0 +1,20 @@
+package io.polyrepo.analyser.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class QueryParameter {
+
+    private int id;
+    private String paramName;
+    private String paramValue;
+    private int queryId;
+
+    public QueryParameter(String paramName, String paramValue, int queryId) {
+        this.paramName = paramName;
+        this.paramValue = paramValue;
+        this.queryId = queryId;
+    }
+}
