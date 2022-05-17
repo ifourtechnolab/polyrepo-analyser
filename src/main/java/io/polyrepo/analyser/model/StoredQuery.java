193ab620b382fdc8d69b31335528273342703fb6
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-17 14:38:03 +0530

    fix: model name

--- src/main/java/io/polyrepo/analyser/model/StoredQuery.java
+++ src/main/java/io/polyrepo/analyser/model/StoredQuery.java
@@ -5,13 +5,13 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class StoredQueries {
+public class StoredQuery {
     private int id;
     private String title;
     private String queryKey;
     private int userId;
 
-    public StoredQueries(String title, String queryKey, int userId) {
+    public StoredQuery(String title, String queryKey, int userId) {
         this.title = title;
         this.queryKey = queryKey;
         this.userId = userId;
