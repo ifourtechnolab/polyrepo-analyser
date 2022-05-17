6f4725b6fdce83cc75f7dab80b8c696ef759028b
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-17 10:48:35 +0530

    feat: save query controller

--- src/main/java/io/polyrepo/analyser/controller/QueryController.java
+++ src/main/java/io/polyrepo/analyser/controller/QueryController.java
@@ -1,17 +1,18 @@
 package io.polyrepo.analyser.controller;
 
 import io.polyrepo.analyser.constant.StringConstants;
+import io.polyrepo.analyser.model.RepoNamesList;
+import io.polyrepo.analyser.model.StoredQueries;
 import io.polyrepo.analyser.service.QueryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -33,6 +34,28 @@ public class QueryController {
             return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (IndexOutOfBoundsException e){
             return new ResponseEntity<>(Collections.singletonList(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"No Stored Queries")),HttpStatus.OK);
+        }
+    }
+
+    /**
+     * Endpoint to store the save query in database
+     * @param title Stored query title
+     * @param userId User id
+     * @param queryKey Query key to identify the analysis
+     * @param repoNamesList List of Repositories selected by user
+     * @param orgName GitHub Organization login name
+     * @param days Number of days for filter
+     * @return ResponseEntity with database operation status
+     */
+    @PostMapping("/saveQuery")
+    public ResponseEntity<Map<String, Object>> saveQuery(@RequestParam String title,@RequestParam int userId,@RequestParam String queryKey, @RequestBody(required = false) RepoNamesList repoNamesList, @RequestParam String orgName, @RequestParam(required = false) Integer days){
+        try {
+            logger.info("Save query of user");
+            return new ResponseEntity<>(queryService.saveQueries(new StoredQueries(title,queryKey,userId), repoNamesList, orgName, days), HttpStatus.OK);
+        }catch (DuplicateKeyException e ){
+            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"Query already exists"),HttpStatus.OK);
+        } catch (SQLException e) {
+            return new ResponseEntity<>(Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"Process Failed"),HttpStatus.OK);
         }
     }
 }
