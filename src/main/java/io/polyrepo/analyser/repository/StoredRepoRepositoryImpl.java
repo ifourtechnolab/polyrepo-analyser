a12ea8ab5c3235497031f63361560cfe2664928e
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-17 17:20:32 +0530

    fix: remove duplicateKeyException of spring starter jdbc

--- src/main/java/io/polyrepo/analyser/repository/StoredRepoRepositoryImpl.java
+++ src/main/java/io/polyrepo/analyser/repository/StoredRepoRepositoryImpl.java
@@ -4,7 +4,6 @@ import io.polyrepo.analyser.model.RepoName;
 import io.polyrepo.analyser.model.RepoNamesList;
 import io.polyrepo.analyser.util.ConnectionUtil;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
@@ -21,11 +20,10 @@ public class StoredRepoRepositoryImpl implements StoredRepoRepository{
      * This method will store the repositories for query in database
      * @param repoNamesList List of Repositories selected by user
      * @param queryId Query id for query reference
-     * @throws DuplicateKeyException if data with same primary key exists in database
      * @throws SQLException if error occurs in database operation
      */
     @Override
-    public void saveRepoNameList(RepoNamesList repoNamesList,int queryId) throws DuplicateKeyException, SQLException {
+    public void saveRepoNameList(RepoNamesList repoNamesList,int queryId) throws SQLException {
         try(Connection connection = ConnectionUtil.getConnection()){
             for (RepoName repo : repoNamesList.getRepoNames()) {
                 try(PreparedStatement preparedStatement = connection.prepareStatement(saveStoredRepoQuery)){
