6e3b4260433002ac0dbe8ccfa7e9880db2b25b28
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-19 18:21:31 +0530

    feat: storedqueries model

---
+++ src/main/java/io/polyrepo/analyser/model/StoredQueryList.java
@@ -0,0 +1,35 @@
+package io.polyrepo.analyser.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class StoredQueryList {
+    private StoredQuery storedQuery;
+    private List<QueryParameter> queryParameterList;
+    private List<QueryRepo> queryRepoList;
+
+    public void createQueryParameterList(QueryParameter queryParameter){
+        this.queryParameterList = new ArrayList<>();
+        queryParameterList.add(queryParameter);
+    }
+
+    public void createQueryRepoList(QueryRepo queryRepo){
+        this.queryRepoList = new ArrayList<>();
+        queryRepoList.add(queryRepo);
+    }
+
+    public void addToQueryParameter(QueryParameter queryParameter){
+        if(!this.queryParameterList.contains(queryParameter))
+            this.queryParameterList.add(queryParameter);
+    }
+
+    public void addToQueryRepoList(QueryRepo queryRepo){
+        if(!this.queryRepoList.contains(queryRepo))
+            this.queryRepoList.add(queryRepo);
+    }
+}
