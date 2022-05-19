2069379cc25777e5ff6167a71988566ec798a099
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-19 18:20:00 +0530

    feat: fetch storedqueries repository

--- src/main/java/io/polyrepo/analyser/repository/QueryRepository.java
+++ src/main/java/io/polyrepo/analyser/repository/QueryRepository.java
@@ -4,14 +4,13 @@ import io.polyrepo.analyser.model.StoredQuery;
 import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Map;
 
 
 @Repository
 public interface QueryRepository {
 
-    List<Map<String, Object>> getStoredQueries(int userId) throws IndexOutOfBoundsException;
+    Map<String, Object> getStoredQueries(int userId) throws SQLException;
 
     int saveStoredQuery(StoredQuery storedQuery)throws SQLException;
 }
