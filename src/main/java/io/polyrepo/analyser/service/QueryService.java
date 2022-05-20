c221f2aafd2ef39077b8dc1c3d836052da89838d
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-20 14:42:52 +0530

    feat: service for stored query result

--- src/main/java/io/polyrepo/analyser/service/QueryService.java
+++ src/main/java/io/polyrepo/analyser/service/QueryService.java
@@ -1,5 +1,6 @@
 package io.polyrepo.analyser.service;
 
+import feign.FeignException;
 import io.polyrepo.analyser.constant.StringConstants;
 import io.polyrepo.analyser.model.QueryParameter;
 import io.polyrepo.analyser.model.RepoNamesList;
@@ -8,6 +9,7 @@ import io.polyrepo.analyser.repository.ParameterRepository;
 import io.polyrepo.analyser.repository.QueryRepository;
 import io.polyrepo.analyser.repository.StoredRepoRepository;
 import io.polyrepo.analyser.util.ParameterName;
+import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
@@ -28,6 +31,15 @@ public class QueryService {
 
     @Autowired
     StoredRepoRepository storedRepoRepository;
+
+    @Autowired
+    private IssueService issueService;
+
+    @Autowired
+    private PullRequestService pullRequestService;
+
+    @Autowired
+    private LabelService labelService;
 
     private final Logger logger = LoggerFactory.getLogger(QueryService.class);
 
@@ -73,12 +85,50 @@ public class QueryService {
         }
     }
 
+    /**
+     * This method will delete the stored query by id
+     * @param queryId id of query to be deleted
+     * @return stored query delete status message
+     * @throws SQLException if error occurs in database operation
+     */
     public Map<String,String> deleteQuery(int queryId) throws SQLException{
         int returnVal = queryRepository.deleteStoredQuery(queryId);
         if(returnVal>0){
             return Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"Query deleted");
         }else{
             return Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"Query not deleted");
+        }
+    }
+
+    /**
+     * This method will call the service of analysis according to the query key
+     * @param token GitHub personal access token
+     * @param queryId Query key to identify the analysis
+     * @param queryKey Query key to identify the analysis
+     * @param orgUserName GitHub Organization login name
+     * @param days Number of days for filter
+     * @param label  Label for filter
+     * @param repoNamesList List of Repositories selected by user
+     * @return result of stored query from the service
+     * @throws FeignException FeignException.Unauthorized if token is invalid, FeignException.BadRequest if FeignClient returns 400 Bad Request
+     * @throws JSONException if JSON parsing is invalid
+     */
+    public Map<String,Object> getQueryResult(String token, int queryId, String queryKey, String orgUserName, Integer days, String label, RepoNamesList repoNamesList) throws FeignException, JSONException {
+        switch (queryKey){
+            case "getPullRequestNotUpdatedByDaysQuery":
+                return pullRequestService.getPRNotUpdatedByDays(token,orgUserName,repoNamesList,days);
+            case "getUnMergedPullRequestByDayQuery":
+                return pullRequestService.getUnMergedPullRequestByDays(token,orgUserName,repoNamesList,days);
+            case "getPriority1IssuesOpenedBeforeXDaysQuery":
+                return issueService.getPriority1IssuesOpenedBeforeXDays(orgUserName,token,repoNamesList,days);
+            case "getOpenIssueNamesByLabel":
+                return labelService.getOpenIssueNamesByLabel(orgUserName,label,token,repoNamesList);
+            case "getClosedP1IssuesTimeQuery":
+                return new HashMap<>(issueService.getAverageResolvingTimeOfP1Issues(orgUserName,token));
+            case "getClosedP2IssuesTimeQuery":
+                return new HashMap<>(issueService.getAverageResolvingTimeOfP2Issues(orgUserName,token));
+            default:
+                return Collections.singletonMap(StringConstants.JSON_MESSAGE_KEY_STRING,"Data not found");
         }
     }
 }
