a92f62bf79e62291fce9c6852627612207fe1d17
Mohammed Awais Memon <awaismemon2323@gmail.com>
2022-05-26 18:59:18 +0530

    feat : add String constants

--- src/main/java/io/polyrepo/analyser/constant/StringConstants.java
+++ src/main/java/io/polyrepo/analyser/constant/StringConstants.java
@@ -13,4 +13,18 @@ public class StringConstants {
     public static final String JSON_REPOSITORIES_KEY="repositories";
     public static final String JSON_REPOSITORY_KEY="Repository";    // "Repository" not "repository"
     public static final String JSON_QUERYKEY_KEY="queryKey";
+    public static final String JSON_PROCESS_FAILED_VALUE="Process Failed";
+    public static final String TABLE_QUERYID_LABEL="q_id";
+    public static final String COLUMN_PARAM_NAME_LABEL="param_name";
+    public static final String COLUMN_PARAM_VALUE_LABEL="param_value";
+    public static final String COLUMN_TITLE_LABEL="title";
+    public static final String COLUMN_BEARER_TOKEN_LABEL="bearer_token";
+    public static final String COLUMN_NAME_LABEL="name";
+    public static final String COLUMN_QUERY_LABEL="query";
+    public static final String COLUMN_IS_TREND_CAPTURED_LABEL="is_trend_captured";
+    public static final String COLUMN_IS_PINNED_LABEL="is_pinned";
+    public static final String COLUMN_DATE_LABEL="date";
+    public static final String COLUMN_RESULT_LABEL="result";
+    public static final String COLUMN_TREND_ID_LABEL="t_id";
+
 }
