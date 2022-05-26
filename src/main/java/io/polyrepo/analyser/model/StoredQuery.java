348d22e89f4f0602e49580589e0f495276bfc3b9
Mohammed Awais Memon <awaismemon2323@gmail.com>
2022-05-26 15:52:00 +0530

    refactor : change variable name 'id' to 'queryId', add boolean isTrendCaptured and isPinned variables

--- src/main/java/io/polyrepo/analyser/model/StoredQuery.java
+++ src/main/java/io/polyrepo/analyser/model/StoredQuery.java
@@ -6,10 +6,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class StoredQuery {
-    private int id;
+    private int queryId;
     private String title;
     private String queryKey;
     private int userId;
+    private boolean isTrendCaptured;
+    private boolean isPinned;
 
     public StoredQuery(String title, String queryKey, int userId) {
         this.title = title;
